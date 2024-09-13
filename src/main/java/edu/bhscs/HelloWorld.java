0ba756cbd4895f7c78f44a9dde1b5e411242d431
ebonyform <1079661@apps.nsd.org>
2024-09-13 18:19:47 +0000

    committ pls

---
+++ src/main/java/edu/bhscs/HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+  // every Java program needs a public main method
+  // for the next while, all code that is written will live inside main()
+  public static void main(String[] args) {
+    // all code in Java sits inside here
+    System.out.println("Hello World!");
+  }
+}
